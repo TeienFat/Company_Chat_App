c8afd9fbba08d1d83512f2e497cafcfb133796c1
KELVIN2K2\hoang <nam.vlh.62cntt@ntu.edu.vn>
2023-11-29 15:18:00 +0700

    design-register

--- lib/screens/auth/register.dart
+++ lib/screens/auth/register.dart
@@ -9,6 +9,65 @@ class RegisterScreen extends StatelessWidget {
       appBar: AppBar(
         title: Text('Đăng kí'),
       ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          // ElevatedButton(onPressed: (){} , child: Icon(Icons.)),
+          SizedBox(
+            height: 60,
+          ),
+          Container(
+            width: 100,
+            height: 100,
+            decoration: BoxDecoration(
+              color: Color.fromARGB(20, 158, 158, 158),
+              borderRadius: BorderRadius.circular(200),
+            ),
+            child: Icon(
+              Icons.person_add_alt_1,
+              size: 70.0,
+            ),
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          Padding(
+            padding: const EdgeInsets.fromLTRB(20, 20, 20, 10),
+            child: TextField(
+              decoration: InputDecoration(
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  hintText: 'Enter Your Email'),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.fromLTRB(20, 10, 20, 20),
+            child: TextField(
+              decoration: InputDecoration(
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  hintText: 'Enter Your Password'),
+            ),
+          ),
+          SizedBox(
+            height: 35,
+          ),
+          Padding(
+            padding: const EdgeInsets.all(20),
+            child: ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                primary: const Color.fromARGB(255, 33, 93, 243), // background
+                onPrimary: Colors.white, // foreground),
+                minimumSize: Size(390, 60),
+              ),
+              onPressed: () {},
+              child: Text('Save', style: TextStyle(fontSize: 17)),
+            ),
+          )
+        ],
+      ),
     );
   }
 }
