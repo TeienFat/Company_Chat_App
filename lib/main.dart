40e52a032f403e0eaf44c6c71a2e82f708395b63
duythien <duythien1310@gmail.com>
2023-11-29 20:23:51 +0700

    "design-chathome-screen"

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,23 @@
+import 'package:company_chat_app_demo/screens/main_screen.dart';
 import 'package:company_chat_app_demo/screens/splash/splash_screen.dart';
 import 'package:company_chat_app_demo/screens/splash/welcome_screen.dart';
 import 'package:company_chat_app_demo/screens/auth/register.dart';
 import 'package:flutter/material.dart';
+import 'package:company_chat_app_demo/firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
+
+
 
 var kColorScheme =
     ColorScheme.fromSeed(seedColor: const Color.fromARGB(1, 207, 46, 46));
 var kDarkColorScheme =
     ColorScheme.fromSeed(seedColor: const Color.fromARGB(1, 207, 46, 46));
-void main() {
+void main() async{
+    WidgetsFlutterBinding.ensureInitialized();
+    await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   runApp(const MyApp());
 }
 
@@ -65,78 +75,11 @@ class MyApp extends StatelessWidget {
             ),
       ),
       // home: const MyHomePage(title: "Chat"),
-      home: const SplashScreen(),
+      home: const MainScreen(),
       routes: <String, WidgetBuilder>{
         '/Login': (BuildContext context) => new RegisterScreen(),
         '/Welcome': (BuildContext context) => new WelcomeScreen()
       },
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        backgroundColor: kColorScheme.primary,
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(
-          Icons.add,
-          color: kColorScheme.onPrimary,
-        ),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
