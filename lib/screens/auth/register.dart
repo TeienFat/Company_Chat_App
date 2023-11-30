130a3f9ee2efc89bffb20ae07cfb0eef3e080fe8
KELVIN2K2\hoang <nam.vlh.62cntt@ntu.edu.vn>
2023-11-30 14:42:41 +0700

    Show gmail and password

--- lib/screens/auth/register.dart
+++ lib/screens/auth/register.dart
@@ -1,72 +1,98 @@
+import 'package:company_chat_app_demo/main.dart';
 import 'package:flutter/material.dart';
 
-class RegisterScreen extends StatelessWidget {
+class RegisterScreen extends StatefulWidget {
   const RegisterScreen({super.key});
 
+  @override
+  State<RegisterScreen> createState() => _RegisterScreenState();
+}
+
+class _RegisterScreenState extends State<RegisterScreen> {
+  String gmail = '';
+  String password = '';
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Đăng kí'),
+        title: Text(
+          'Đăng Kí',
+          style: TextStyle(color: kColorScheme.onPrimary),
+        ),
       ),
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          // ElevatedButton(onPressed: (){} , child: Icon(Icons.)),
-          SizedBox(
-            height: 60,
-          ),
-          Container(
-            width: 100,
-            height: 100,
-            decoration: BoxDecoration(
-              color: Color.fromARGB(20, 158, 158, 158),
-              borderRadius: BorderRadius.circular(200),
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            // ElevatedButton(onPressed: (){} , child: Icon(Icons.)),
+            SizedBox(
+              height: 60,
             ),
-            child: Icon(
-              Icons.person_add_alt_1,
-              size: 70.0,
-            ),
-          ),
-          SizedBox(
-            height: 20,
-          ),
-          Padding(
-            padding: const EdgeInsets.fromLTRB(20, 20, 20, 10),
-            child: TextField(
-              decoration: InputDecoration(
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(10),
-                  ),
-                  hintText: 'Enter Your Email'),
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.fromLTRB(20, 10, 20, 20),
-            child: TextField(
-              decoration: InputDecoration(
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(10),
-                  ),
-                  hintText: 'Enter Your Password'),
-            ),
-          ),
-          SizedBox(
-            height: 35,
-          ),
-          Padding(
-            padding: const EdgeInsets.all(20),
-            child: ElevatedButton(
-              style: ElevatedButton.styleFrom(
-                primary: const Color.fromARGB(255, 33, 93, 243), // background
-                onPrimary: Colors.white, // foreground),
-                minimumSize: Size(390, 60),
+            Container(
+              width: 100,
+              height: 100,
+              decoration: BoxDecoration(
+                color: Color.fromARGB(20, 158, 158, 158),
+                borderRadius: BorderRadius.circular(200),
               ),
-              onPressed: () {},
-              child: Text('Save', style: TextStyle(fontSize: 17)),
+              child: Icon(
+                Icons.person_add_alt_1,
+                size: 70.0,
+              ),
             ),
-          )
-        ],
+            SizedBox(
+              height: 20,
+            ),
+            Padding(
+              padding: const EdgeInsets.fromLTRB(20, 20, 20, 10),
+              child: TextFormField(
+                decoration: InputDecoration(
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    hintText: 'Enter Your Email'),
+                onChanged: (text) {
+                  gmail = text;
+                },
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.fromLTRB(20, 10, 20, 20),
+              child: TextFormField(
+                decoration: InputDecoration(
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    hintText: 'Enter Your Password'),
+                onChanged: (text) {
+                  password = text;
+                },
+              ),
+            ),
+            SizedBox(
+              height: 35,
+            ),
+            Padding(
+              padding: const EdgeInsets.all(20),
+              child: ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  primary: kColorScheme.primary,
+                  onPrimary: kColorScheme.onPrimary,
+                  minimumSize: Size(390, 60),
+                ),
+                onPressed: () {
+                  setState(() {
+                    Text('$gmail');
+                    Text('$password');
+                  });
+                },
+                child: Text('Save', style: TextStyle(fontSize: 17)),
+              ),
+            ),
+            Text('$gmail'),
+            Text('$password'),
+          ],
+        ),
       ),
     );
   }
