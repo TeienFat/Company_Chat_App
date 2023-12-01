f7ff38a2e9b694936a391ae9cd0817b20222e4e2
KELVIN2K2\hoang <nam.vlh.62cntt@ntu.edu.vn>
2023-12-02 01:35:30 +0700

    Giao diện và code welcome nhưng phần hình ảnh vẫn còn lỗi

--- lib/screens/Splash/welcome_screen.dart
+++ lib/screens/Splash/welcome_screen.dart
@@ -1,4 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:carousel_slider/carousel_slider.dart';
+
+final List<String> imgList = [
+  'Image.asset("assets/images/hinh1.png")',
+  'Image.asset("assets/images/hinh2.png")',
+  'Image.asset("assets/images/hinh3.png")',
+];
 
 class WelcomeScreen extends StatefulWidget {
   const WelcomeScreen({super.key});
@@ -11,9 +18,41 @@ class _WelcomeScreenState extends State<WelcomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text("Welcome to the app"),
-      ),
-    );
+        appBar: AppBar(
+          centerTitle: true,
+          title: Text(
+            'Wido Chat',
+            style: TextStyle(fontSize: 19),
+          ),
+        ),
+        body: Container(
+          child: Column(
+            children: [
+              Padding(
+                padding: EdgeInsets.all(20),
+              ),
+              CarouselSlider(
+                  items: imgList
+                      .map(
+                        (item) => Container(
+                          child: Image.asset(
+                            item,
+                            width: 20,
+                          ),
+                        ),
+                      )
+                      .toList(),
+                  options: CarouselOptions(
+                      autoPlay: true,
+                      aspectRatio: 2.0,
+                      enlargeCenterPage: true,
+                      height: 400)),
+              SizedBox(
+                height: 250,
+              ),
+              ElevatedButton(onPressed: () {}, child: Text('Bắt Đầu'))
+            ],
+          ),
+        ));
   }
 }
