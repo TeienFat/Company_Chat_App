4cbb5ae92fe43c26d3c46c19b1601be8645ea59b
KELVIN2K2\hoang <nam.vlh.62cntt@ntu.edu.vn>
2023-12-06 14:47:06 +0700

    XEM THÔNG TIN CÁ NHÂN

--- lib/screens/profile.dart
+++ lib/screens/profile.dart
@@ -1,4 +1,6 @@
+import 'package:company_chat_app_demo/main.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class ProfileScreen extends StatefulWidget {
   const ProfileScreen({super.key});
@@ -10,6 +12,134 @@ class ProfileScreen extends StatefulWidget {
 class _ProfileScreenState extends State<ProfileScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Text('Profile');
+    return Container(
+      width: double.infinity,
+      child: Center(
+        child: Column(
+          children: [
+            Padding(padding: EdgeInsets.all(20)),
+            Image.asset(
+              'assets/images/user.png',
+              height: 100,
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Text(
+              'Vũ Lê Hoàng Nam',
+              style: TextStyle(fontSize: 27, fontWeight: FontWeight.bold),
+            ),
+            SizedBox(
+              height: 30,
+            ),
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                SizedBox(
+                  width: 15,
+                ),
+                Text(
+                  'Thông tin cá nhân',
+                  style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+                ),
+              ],
+            ),
+            SizedBox(
+              height: 30,
+            ),
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                SizedBox(
+                  width: 15,
+                ),
+                Text(
+                  'Điện thoại : 0935767192',
+                  style: TextStyle(fontSize: 20),
+                ),
+              ],
+            ),
+            SizedBox(
+              height: 30,
+            ),
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                SizedBox(
+                  width: 15,
+                ),
+                Text(
+                  'Giới tính : Nam',
+                  style: TextStyle(fontSize: 20),
+                ),
+              ],
+            ),
+            SizedBox(
+              height: 30,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                SizedBox(
+                  width: 15,
+                ),
+                Flexible(
+                  child: Text(
+                    'Ngày tháng năm sinh : 01 tháng 09 năm 2002',
+                    style: TextStyle(
+                      fontSize: 20,
+                    ),
+                    maxLines: 2,
+                    overflow: TextOverflow.ellipsis,
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(
+              height: 30,
+            ),
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                SizedBox(
+                  width: 15,
+                ),
+                Text(
+                  'Địa chỉ : Nha Trang',
+                  style: TextStyle(fontSize: 20),
+                ),
+              ],
+            ),
+            SizedBox(height: 95),
+            ElevatedButton(
+              onPressed: () {},
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Icon(
+                    Icons.edit_document,
+                    size: 30.0,
+                  ),
+                  SizedBox(
+                    width: 10,
+                  ),
+                  Text(
+                    'Cập nhật thông tin',
+                    style: TextStyle(
+                      fontSize: 20,
+                    ),
+                  ),
+                ],
+              ),
+              style: ElevatedButton.styleFrom(
+                fixedSize: Size(380, 50),
+                primary: kColorScheme.primary,
+                onPrimary: kColorScheme.onPrimary,
+              ),
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
